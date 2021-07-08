f11dcb3d4b2e45b75208f9dc141c59ca51320c7b
oav <ortizqtv@gmail.com>
2021-07-07 22:34:20 -0500

    added an arr of func obj && obj call func

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,32 @@
+//calc
+
+//operator functions
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const half = (a) => 1 / a;
-const percentage = (a) => a / 100;
+const fraction = (a) => 1 / a;
+const percentage = (a, b) => (b / 100) * a;
 const square = (a, b) => a ** b;
-let root = (a) => {
-  let lower = 0;
-  let upper = a;
-  while (lower <= upper) {
-    let mid = Math.floor((lower + upper) / 2);
-    if (mid * mid <= a) {
-      lower = mid + 1;
-    } else {
-      upper = mid - 1;
-    }
-  }
-  return upper;
+const root = (a) => Math.sqrt(a);
+
+const operators = [
+  { add: add },
+  { subtract: subtract },
+  { multiply: multiply },
+  { divide: divide },
+  { fraction: fraction },
+  { percentage: percentage },
+  { square: square },
+  { root: root },
+];
+
+//operator call function
+
+const operate = (a, input, b = 0) => {
+  let result = operators.find((operator) => operator[input]);
+  return result[input](a, b);
 };
 
-
-let root = (a) => {
-    let avg = (a, b) => (a + b)/2, c=5, b;
-    for (let i = 0; i <= 25; i++){
-    b = a/c 
-    c = avg(b, c)
-    }
-    return c
-}
+//
