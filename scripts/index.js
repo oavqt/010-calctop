e923a16c20f57324be696f32787a31534594eb6e
oav <ortizqtv@gmail.com>
2021-07-08 02:44:59 -0500

    css updates

--- scripts/index.js
+++ scripts/index.js
@@ -4,36 +4,66 @@ window.onload = function () {
   content();
   calculator();
   description();
+  calcContent();
 };
 
 //create content interface
 
 function content() {
-  const content = document.querySelector('.content');
+  const content = document.querySelector('.container');
   const calculator = document.createElement('div');
   const description = document.createElement('div');
-  calculator.className = 'content__calculator';
-  description.className = 'content__description';
+  calculator.className = 'container__calculator';
+  description.className = 'container__description';
   content.appendChild(calculator);
   content.appendChild(description);
 }
 
 function calculator() {
-  const calculator = document.querySelector('.content__calculator');
+  const calculator = document.querySelector('.container__calculator');
+  const bar = document.createElement('div');
+  const content = document.createElement('div');
+  bar.className = 'container__calculator__bar';
+  content.className = 'container__calculator__content';
+  calculator.appendChild(bar);
+  calculator.appendChild(content);
 }
 
 function description() {
-  const description = document.querySelector('.content__description');
+  const description = document.querySelector('.container__description');
   const title = document.createElement('h1');
-  title.className = 'content__description__title';
   const information = document.createElement('p');
-  information.className = 'content__description__information';
   const button = document.createElement('button');
-  button.className = 'content__description__button';
   const tags = document.createElement('a');
-  tags.className = 'content__description__tags';
+  title.className = 'container__description__title';
+  information.className = 'container__description__information';
+  button.className = 'container__description__button';
+  tags.className = 'container__description__tags';
   description.appendChild(title);
   description.appendChild(information);
   description.appendChild(button);
   description.appendChild(tags);
 }
+
+function calcContent() {
+  const bar = document.querySelector('.container__calculator__bar');
+  const title = document.createElement('h1');
+  title.className = 'container__calculator__bar__title';
+  title.textContent = 'Calculator';
+  bar.appendChild(title);
+  for (let i = 0; i < 3; i++) {
+    const button = document.createElement('button');
+    button.className = 'container__calculator__bar__button';
+    bar.appendChild(button);
+  }
+
+  const content = document.querySelector('.container__calculator__content');
+  const display = document.createElement('div');
+  const history = document.createElement('div');
+  display.className = 'container__calculator__content__display';
+  history.classList = 'container__calculator__content__history';
+  content.appendChild(display);
+  content.appendChild(history);
+}
+
+//
